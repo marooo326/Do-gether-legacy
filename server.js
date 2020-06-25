1b00f7ee23fe31f71a83b5bddcd5de845a2e0662
김대휘 <dhxl50@khu.ac.kr>
2020-06-25 11:23:58 +0900

    bcrypt 암호화

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
 
 const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
@@ -33,8 +34,18 @@ app.get("/api/cards", (req, res) => {
 
 app.post("/api/addcard", (req, res) => {
   const data = req.body;
-  const sql = "INSERT INTO CARDINFO(isPublic,name,date,time,title,todo,ck) VALUES(?,?,?,?,?,?,?);";
-  const params =[data.isPublic,data.name,data.date,data.time,data.title,data.todo,data.ck];
+  const sql =
+    "INSERT INTO CARDINFO(isPublic,name,date,time,title,todo,ck) VALUES(?,?,?,?,?,?,?);";
+
+  const params = [
+    data.isPublic,
+    data.name,
+    data.date,
+    data.time,
+    data.title,
+    data.todo,
+    data.ck,
+  ];
   connection.query(sql, params, (err, rows, fields) => {
     if (err) {
       res.send({
@@ -50,25 +61,62 @@ app.post("/api/addcard", (req, res) => {
   });
 });
 
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
   const data = req.body;
-  
-  console.log(data);
   const sql = "INSERT INTO USERINFO(userID,userPW,userName) VALUES(?,?,?);";
-  const params =[data.userID,data.userPW,data.userName];
-  connection.query(sql, params, (err, rows, fields) => {
-    if (err) {
-      res.send({
-        code: 400,
-        message: "error",
-      });
-    } else {
-      res.send({
-        code: 200,
-        message: "success",
-      });
-    }
+  
+  await bcrypt.hash(data.userPW, saltRounds, function (err, hash) {
+    let params = [data.userID, hash, data.userName];
+    connection.query(sql, params, (err, rows, fields) => {
+      if (err) {
+        res.send({
+          code: 400,
+          message: "error",
+        });
+      } else {
+        res.send({
+          code: 200,
+          message: "success",
+        });
+      }
+    });
   });
 });
+
+app.post("/api/login", (req, res) => {
+  const data = req.body;
+  const enteredID = data.userID;
+  const enteredPW = data.userPW;
+  connection.query('SELECT * FROM USERINFO WHERE userID = ?', [enteredID],
+  function( error, results, fields) {
+      if (error) {
+          // console.log("error ocurred", error);
+          res.send({
+              "code": 400,
+              "failed": "error ocurred"
+          })
+      } else {
+          // console.log('The solution is: ', results);
+          if(results.length > 0) {
+              if(results[0].userID == password) {
+                  res.send({
+                      "code": 200,
+                      "success": "login sucessfull"
+                  });
+              } else {
+                  res.send({
+                      "code": 204,
+                      "success": "Email and password does not match"
+                  });
+              }
+          } else {
+              res.send({
+                  "code":204,
+                  "success": "Email does not exists"
+              });
+          }
+      }    
+  }) 
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
